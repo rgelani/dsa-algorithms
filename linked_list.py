f6c7733b904b30a02e4f9e7b3b4e5f5c64b3f98a
rgelani <info.rgelani@gmail.com>
2025-11-28 13:42:30 -0500

    Add linked list

---
+++ linked_list.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+n1 = Node(10)
+n2 = Node(20)
+n3 = Node(30)
+
+n1.next = n2
+n2.next = n3
+n3.next = None
+
+current = n1
+while current.next != None:
+    print(current.data)
+    current = current.next
